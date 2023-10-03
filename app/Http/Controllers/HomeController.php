e99c50ee1fe8b8ed0dac7e9ce5baed2fa1dee84f
Elvin.Memmedov <Elvin.Memmedov@abc-telecom.az>
2023-10-03 09:23:00 +0400

    refactor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,19 +8,20 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $data = [12,8,3,20,22,8,23];
+        $data = [12,25,8,3,40,20,23,22,8,30];
         $result = [];
         for ($i = 0;$i < count($data); $i++)
         {
-            $elem = $data[$i];
+            $resultKey = $data[$i];
+            $resultValue = -1;
             for ($j = $i; $j < count($data); $j++)
             {
-                if ($data[$j] > $elem){
-                    $temp = [$elem => $j];
-                    $result[] = $temp;
+                if ($data[$j] > $resultKey){
+                    $resultValue = $j;
                     break;
                 }
             }
+            $result[] = [$resultKey => $resultValue];
         }
         return $result;
     }
